3b9fa61c5a7f63c69f416c8ef197549e18ca000b
oobiwanjanodi <111980342+oobiwanjanodi@users.noreply.github.com>
2024-04-22 09:43:36 +0800

    comments for migration use

--- app/models.py
+++ app/models.py
@@ -7,6 +7,10 @@ import sqlalchemy.orm as so
 from app import db, login
 from hashlib import md5
 
+# flask db migrate - generates a migration script (needs to be incorporated into git source control idk how yet)
+# flask db upgrade - updates the database but doesn't destroy any existing data
+
+
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)

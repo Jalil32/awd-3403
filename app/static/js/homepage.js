34dd6d0da4323a82915b8cef9b77f07850f86479
oobiwanjanodi <111980342+oobiwanjanodi@users.noreply.github.com>
2024-04-27 17:27:31 +0800

    functioning popup box, centered and scrollable

--- app/static/js/homepage.js
+++ app/static/js/homepage.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('intro-text').textContent = `Ready to plate up, ${localStorage.getItem('username')}? Share a recent cook or rate last night’s dinner!`;
 });
 
-function togglePopup() { 
-    const overlay = document.getElementById('popupOverlay'); 
-    overlay.classList.toggle('show'); 
-} 
+function togglePopup() {
+    var overlay = document.getElementById('popupOverlay');
+    overlay.style.visibility = overlay.style.visibility === 'visible' ? 'hidden' : 'visible';
+}

b25c5536e219f63d991bf0739d2a68150b31d725
oobiwanjanodi <111980342+oobiwanjanodi@users.noreply.github.com>
2024-05-02 12:17:36 +0800

    review view fixed no label left behind

--- app/static/js/homepage.js
+++ app/static/js/homepage.js
@@ -18,6 +18,7 @@ window.addEventListener('scroll', function() {
 
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('intro-text').textContent = `Ready to plate up, ${localStorage.getItem('username')}? Share a recent cook or rate last night’s dinner!`;
+    toggleRating()
 });
 
 function togglePopup() {
@@ -26,19 +27,19 @@ function togglePopup() {
 }
 
 function toggleRating() {
-    var ratingStars = document.getElementById('ratingStars');
+    var rating = document.querySelector('.inline-rating');
     var postBody = document.getElementById('post-body');
     var postPicture = document.getElementById('post_picture');
     var upload = document.getElementById('upload_picture');
 
     if (document.getElementById('Request').checked) {
-        ratingStars.style.display = 'none';
+        rating.style.display = 'none';
         postPicture.style.display = 'none';
         upload.style.display = 'none';
         postBody.placeholder = "Find a plate! Tell us what you're looking for.";
 
     } else {
-        ratingStars.style.display = 'block';
+        rating.style.display = 'flex';
         postPicture.style.display = 'block';
         upload.style.display = 'block';
         postBody.placeholder = "Leave a review!!";
